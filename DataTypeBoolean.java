03bc8724e01c101660ff3b03268572fbafcb469a
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 23:36:40 +0000

    Boolean Commit

---
+++ DataTypeBoolean.java
@@ -0,0 +1,16 @@
+package datatypeboolean;
+
+
+
+public class DataTypeBoolean
+{
+    public static void main(String[] args)
+
+    {
+        boolean A = true;
+        boolean B = false;
+
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
